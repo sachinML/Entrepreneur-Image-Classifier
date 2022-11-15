03f0c780e7fa5a724376d081976b3ea04ab0201f
Sachin Jha <Sjha979@gmail.com>
2022-11-15 21:48:58 +0530

    Update server.py

--- server/server.py
+++ server/server.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 def classify_image():
     image_data = request.form['image_data']
     response = jsonify(util.classify_image(image_data))
-
     response.headers.add('Access-Control-Allow-Origin', '*')
 
     return response
